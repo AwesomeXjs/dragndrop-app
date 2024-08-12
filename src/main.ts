4dd29d27c185924ba3bc2e9b118c9dbd654b8451
awesomex <bestawes0mex@gmail.com>
2024-08-12 11:15:25 +0500

    init app

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import '@/scss/_style.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+export default app
+app.mount('#app')
